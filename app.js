981bc94bd02a8c963fbd6f52a86ededfdeca39e7
A-M-I <160208713+Alisha000111@users.noreply.github.com>
2024-04-12 16:42:14 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+for (var i = 5; i <= 15; i++) {
+    console.log(i);
+}
+var count = 0;
+while (count < 5) {
+    console.log("Hello, Pakistan!");
+    count++;
+}
+var favoriteMovies = [
+    "12 angry men",
+    "Avatar",
+    "Titanic",
+    "Top gun",
+];
+for (var _i = 0, favoriteMovies_1 = favoriteMovies; _i < favoriteMovies_1.length; _i++) {
+    var movie = favoriteMovies_1[_i];
+    console.log(movie);
+}
---
+++ app.ts
@@ -0,0 +1,18 @@
+for (let i = 5; i <= 15; i++) {
+    console.log(i); 
+  }
+let count: number = 0;
+while (count < 5) {
+  console.log("Hello, Pakistan!"); 
+  count++; 
+}
+const favoriteMovies: string[] = [
+    "12 angry men",
+    "Avatar",
+    "Titanic",
+    "Top gun",
+  ];
+  for (const movie of favoriteMovies) {
+    console.log(movie);
+  }
+  
